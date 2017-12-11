e488b4c8655481bd64e22daf1a64adcc0c408459
Иван Хомичков <ivan.khomichkov@gmail.com>
2017-12-11 16:25:30 +0300

    Finished student class dataanotation.

--- ContosoUniversityMVC2/Models/Student.cs
+++ ContosoUniversityMVC2/Models/Student.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ContosoUniversityMVC2.Models
 {
     public class Student
     {
         public int ID { get; set; }
-        [Display(Name ="Last Name")]
+        [Display(Name ="Last Name"), StringLength(50)]
         public string LastName { get; set; }
-        [Display(Name = "First Name / Mid Name")]
+        [Display(Name = "First Name / Mid Name"),StringLength(50,ErrorMessage ="First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         [Display(Name = "Enrollment Date"),DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
